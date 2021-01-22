e527b2a5151f7483ed6577cce171a07cf4f5e32d
Tyler Shambora <tyler@theshamboras.com>
2021-01-22 15:32:23 -0800

    Delete packages/markets/bundle directory

--- packages/markets/bundle/packages/@services/debug/index.js
+++
@@ -1,6 +0,0 @@
-const trace = require('@google-cloud/trace-agent');
-const debug = require('@google-cloud/debug-agent');
-
-trace.start();
-
-debug.start({ serviceContext: { enableCanary: true } });
--- packages/markets/bundle/packages/@services/env/index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({
-    path: '/Users/tylershambora/Code/Personal/pav2tty5lo7geycf/.env',
-  });
-}
