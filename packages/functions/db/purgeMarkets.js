de70424f5408b97c95a3a0dc623242da95d17ae3
tshamz <tyler@theshamboras.com>
2021-04-17 15:33:32 -0600

    delete missing ids

--- packages/functions/db/purgeMarkets.js
+++ packages/functions/db/purgeMarkets.js
@@ -1,8 +1,16 @@
 const firebase = require('@services/firebase');
+const predictit = require('@services/predictit');
 
 module.exports = async (context, res) => {
   try {
     const marketsRef = firebase.db.ref('markets');
+    const snapshot = await marketsRef.once('value');
+    const firebaseIds = Object.keys(await snapshot.val());
+    const predictitIds = await predictit
+      .fetchAllMarkets()
+      .then((markets) => markets.map(({ id }) => `${id}`));
+
+    const missingIds = firebaseIds.filter((id) => !predictitIds.includes(id));
 
     const inactiveSnapshot = await marketsRef
       .orderByChild('active')
@@ -15,7 +23,7 @@ module.exports = async (context, res) => {
       .endAt(0)
       .once('value');
 
-    const keysToDelete = [];
+    const keysToDelete = [...missingIds];
 
     negativeDaysLeft.forEach((snapshot) => keysToDelete.push(snapshot.key));
     inactiveSnapshot.forEach((snapshot) => keysToDelete.push(snapshot.key));
